b9b63300b31d2cfd7e57112d340e66a944d60ceb
Dhruv Suri <dhruvsuri789@gmail.com>
2024-11-25 10:58:46 +0530

    Added not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,21 @@
+import ButtonLink from "./_components/ButtonLink";
+
 function NotFoundPage() {
-  return <main>Not found</main>;
+  return (
+    <main>
+      <div className="max-w-[600px] mx-auto flex flex-col items-center py-24 gap-8">
+        <h1 className="text-center text-4xl font-bold">
+          The page you are looking for{" "}
+          <span className="text-red-600">does not exist</span>
+        </h1>
+        <div className="flex gap-4">
+          <ButtonLink href="/" varient="primary">
+            Go back home
+          </ButtonLink>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default NotFoundPage;
